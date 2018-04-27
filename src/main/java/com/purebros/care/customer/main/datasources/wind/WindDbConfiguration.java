64c0cef8ce915a6cc7cb01742b9959a1edc23e76
@paultretiak <pavel.t@advantagemedia.agency>
2018-04-27 17:11:10 +0200

    add configuration for wind database

---
+++ src/main/java/com/purebros/care/customer/main/datasources/wind/WindDbConfiguration.java
@@ -0,0 +1,61 @@
+package com.purebros.care.customer.main.datasources.wind;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.purebros.care.customer.main.datasources.wind",
+        entityManagerFactoryRef = "windEntityManager",
+        transactionManagerRef = "windTransactionManager"
+)
+public class WindDbConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean windEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(windDataSource());
+        em.setPackagesToScan("com.purebros.care.customer.main.datasources.wind");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager windTransactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    public DataSource windDataSource() {
+
+        System.out.println("<<<<<< Connection to wind database >>>>>> "
+            + "class: " + env.getProperty("wind.datasource.driver-class-name")
+            + "; url: " + env.getProperty("wind.datasource.url")
+            + "; username: " + env.getProperty("wind.datasource.username")
+            + "; password: " + env.getProperty("wind.datasource.password")
+        );
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("wind.datasource.driver-class-name"));
+        dataSource.setUrl(     env.getProperty("wind.datasource.url"));
+        dataSource.setUsername(env.getProperty("wind.datasource.username"));
+        dataSource.setPassword(env.getProperty("wind.datasource.password"));
+
+        return dataSource;
+    }
+}
