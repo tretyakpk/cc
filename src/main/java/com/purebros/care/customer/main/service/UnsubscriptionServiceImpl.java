a528bce420c46f22502de1119897bff479f808d9
tretyakpk <pavel.t@advantagemedia.agency>
2018-05-09 17:01:38 +0200

    views

--- src/main/java/com/purebros/care/customer/main/service/UnsubscriptionServiceImpl.java
+++ src/main/java/com/purebros/care/customer/main/service/UnsubscriptionServiceImpl.java
@@ -2,21 +2,30 @@ package com.purebros.care.customer.main.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@PropertySource("error_mapping_unsubscroption_servlet.properties")
 public class UnsubscriptionServiceImpl implements UnsubscriptionService {
 
+    private final Environment env;
+
     private final static Logger logger = LoggerFactory.getLogger(UnsubscriptionServiceImpl.class);
+
+    public UnsubscriptionServiceImpl(Environment env) {
+        this.env = env;
+    }
 
     @Override
     public String deactivate(String url) {
         try {
             RestTemplate restTemplate = new RestTemplate();
             String result = restTemplate.getForObject(url, String.class);
-            return "Success: return code: '" + result + "'";
+            return "Return code: " + result + ". Message: '" + env.getProperty(result) + "'";
         } catch (HttpServerErrorException e) {
             return "Service temporary unavailable please try again later: '" + e.getMessage() + "'";
         }
